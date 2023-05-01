d4cb0badf5246f2201ced77424b7cde3bda2093c
Shams <skm478@gmail.com>
2023-05-01 13:14:52 -0400

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+const add = (param1,param2) => param1+param2;
+const square = param1 => param1*param1;
+
+/*function square(param){
+    return (param*param)
+} */
+
+function divide(x,y){
+    return  x/y
+}
+
